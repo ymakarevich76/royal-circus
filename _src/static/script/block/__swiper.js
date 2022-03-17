a3492c0ed5fe15fb183e1e476fd185f39708b191
Юля <ymakarevich76@gmail.com>
2022-03-17 16:07:30 +0300

    Add footer

--- _src/static/script/block/__swiper.js
+++ _src/static/script/block/__swiper.js
@@ -1,11 +1,25 @@
 const swiper = new Swiper('.cards__slider', {
-  navigation: {
-    prev: '.cards__slider-btn-next',
-    next: '.cards__slider-btn-next',
-  },
   slidesPerView: 3,
 
   loop: true,
   spaceBetween: 60,
 
+  navigation: {
+    prevEl: '.cards__slider-btn-prev',
+    nextEl: '.cards__slider-btn-next',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    },
+    1199: {
+      slidesPerView: 3,
+      spaceBetween: 60,
+    }
+  }
 });
